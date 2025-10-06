2ac30e84bab0ebd784eebd863ec37a1a7db45909
yourusernamenamehafybufya <hafsaabdifitah06@gmail.com>
2025-10-06 21:07:47 +0100

    Turned calender_printer into a function

--- calenderPrinter.py
+++ calenderPrinter.py
@@ -1,21 +1,27 @@
 
+    #take user input: number of days in month
 days_in_month = int(input("How many days are in the month? "))
+
+    #take user input: day the month starts on
 first_day = int(input("What day of the week does the month start on? (Sun=1, Monday=2, ... Sat= 7) "))
 
-counter = 0 #counts the number of variables 
-#def calender_printer(days_in_month, first_day):
+ # function to print calendar
+def calender_printer(days_in_month, first_day):
 
-calender_title = ["S", "M" , "T", "W", "T", "F", "S"]
-print("  ".join(calender_title))
+    calender_title = ["S", "M" , "T", "W", "T", "F", "S"]
+    print("  ".join(calender_title)) 
 
-for i in range(first_day-1):
-    print("-  ", end="") #ensure it prints on same line
+         #makes spaces based on first day of month
+    for i in range(first_day-1):
+        print("   ", end="") # end = "" ensure it prints on same line
 
-for i in range(1, days_in_month+1):
-    print(f"{i:2}", end=" ")
+        #takes number of days in the month
+    for i in range(1, days_in_month+1): 
+        print(f"{i:2}", end=" ") 
 
-    if (i + first_day - 1) % 7 == 0:
-        print()  # newline
+             #new line after every 7th day (including spaces)
+        if (i + first_day - 1) % 7 == 0:
+            print()  # newline
 
-
-#print_gaps based on the day user selected
+#calls function
+calender_printer(days_in_month, first_day)
