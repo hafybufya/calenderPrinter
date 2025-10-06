eac7bb7d160faf136bacd41a454c2d58943f24e3
yourusernamenamehafybufya <hafsaabdifitah06@gmail.com>
2025-10-06 11:42:40 +0100

    comitting empty function as so far

---
+++ calenderPrinter.py
@@ -0,0 +1,6 @@
+
+def days_in_month_calender_printer(days_in_month, first_day):
+    pass
+days_in_month = int(input("How many days are in the month? "))
+first_day = int(input("What day of the week does the month start on? (Sun=0, Monday=1, ... Sat= 6) "))
+
