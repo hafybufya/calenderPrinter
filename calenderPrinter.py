bf4e009054fb626d156994d9f51a109d169a67b4
yourusernamenamehafybufya <hafsaabdifitah06@gmail.com>
2025-10-06 11:55:47 +0100

    Created a for loop to create gaps in calendar

--- calenderPrinter.py
+++ calenderPrinter.py
@@ -1,6 +1,15 @@
 
-def days_in_month_calender_printer(days_in_month, first_day):
-    pass
+import numpy as np
 days_in_month = int(input("How many days are in the month? "))
 first_day = int(input("What day of the week does the month start on? (Sun=0, Monday=1, ... Sat= 6) "))
+
+def calender_printer(days_in_month, first_day):
+
+    calender_title = np.array(["Sun", "Mon" , "Tue", "Wed", "Thur", "Fri", "Sat"])
+
+    for i in range(first_day):
+        print(" ", end = "") #ensure it prints on same line
+
+    return calender_title
+#print_gaps based on the day user selected
 
